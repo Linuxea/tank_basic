704c11c2452d0e00b079e0c5c90e14dd2c49172e
linuxea <5201314123abc>
2017-11-03 08:10:55 +0800

    Initial commit

---
+++ src/main/java/com/basictank/ClientMain.java
@@ -0,0 +1,65 @@
+package com.basictank;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Linuxea on 11/3/17.
+ */
+
+public class ClientMain extends Frame {
+
+    private static int TANK_OFFSET_X = 0;
+    private static int TANK_OFFSET_Y = 0;
+
+    @Override
+    public void paint(Graphics g) {
+        Color oldColor = g.getColor();
+        g.setColor(Color.red);
+        g.fillOval(50 + TANK_OFFSET_X, 50 + TANK_OFFSET_Y, 30, 30);
+        g.setColor(oldColor);
+
+        this.move();
+    }
+
+    private void init(){
+        super.setLocation(400, 300);
+        super.setSize(800, 600);
+        super.setVisible(true);
+        super.setResizable(false);
+        super.setTitle("tank basic version");
+        super.setAlwaysOnTop(true);
+        super.setBackground(Color.green);
+        super.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                System.exit(0);
+            }
+        });
+
+    }
+
+    private void move(){
+        ClientMain.TANK_OFFSET_X += 10;
+        ClientMain.TANK_OFFSET_Y += 30;
+    }
+
+    private void launch(){
+        this.init();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ClientMain main = new ClientMain();
+        main.launch();
+
+        while(true){
+            main.repaint();
+            TimeUnit.MILLISECONDS.sleep(50);
+        }
+
+    }
+
+}
